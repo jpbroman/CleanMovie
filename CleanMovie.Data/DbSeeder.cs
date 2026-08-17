ae95541ac8ff9cc4b65e127ba8b9af1786c3a1fc
Peter Broman <jp.broman@telia.com>
2026-08-17 11:21:58 +0200

    Fixar sökning/filtrering och seedar mer data

--- CleanMovie.Data/DbSeeder.cs
+++ CleanMovie.Data/DbSeeder.cs
@@ -7,6 +7,7 @@ public static class DbSeeder
 {
     public static void Seed(MovieDbContext context)
     {
+        // 1. OM DET REDAN FINNS DATA, TÖM ALLT FÖR EN FRÄSCH OMSTART
         if (context.Movies.Any())
         {
             // Töm alla relaterade tabeller först (så att inga främmande nycklar kraschar)
@@ -18,64 +19,49 @@ public static class DbSeeder
             context.SaveChanges();
 
             // 2. NOLLSTÄLL ID-RÄKNAREN I SQLITE:
-            // Detta kommando nollställer autoincrement-räknaren för ALLA tabeller i SQLite
             context.Database.ExecuteSqlRaw("DELETE FROM sqlite_sequence;");
         }
 
-        context.Movies.RemoveRange(context.Movies);
-        context.SaveChanges();
-
-       // 1. Kontrollera om det redan finns filmer i databasen för att undvika dubbletter
-        if (context.Movies.Any())
-        {
-            return; 
-        }
-
-        // 2. Skapa några fristående skådespelare som vi kan återanvända
-        var actor1 = new Actor { Name = "Keanu Reeves", BirthDate = "1961-07-01" };
-        var actor2 = new Actor { Name = "Laurence Fishburne", BirthDate = "1957-06-22" };
-        var actor3 = new Actor { Name = "Sigourney Weaver", BirthDate = "1959-02-12" };
-        var actor4 = new Actor { Name = "Morgan Freeman", BirthDate = "1951-11-13" };
-        var actor5 = new Actor { Name = "Tim Robbins", BirthDate = "1999-05-11" };
-        var actor6 = new Actor { Name = "Leonardo DiCaprio", BirthDate = "1972-09-18" };
-        var actor7 = new Actor { Name = "Joseph Gordon-Levitt", BirthDate = "1967-06-01" };
-        var actor8 = new Actor { Name = "Peter Broman", BirthDate = "1969-06-01" };
+        // 3. SKAPA SKÅDESPELARE (Objekten sparas i minnet och tilldelas ID automatiskt av EF)
+        var actor1 = new Actor { Name = "Keanu Reeves", BirthDate = "1964-09-02" };
+        var actor2 = new Actor { Name = "Laurence Fishburne", BirthDate = "1961-07-30" };
+        var actor3 = new Actor { Name = "Sigourney Weaver", BirthDate = "1949-10-08" };
+        var actor4 = new Actor { Name = "Morgan Freeman", BirthDate = "1937-06-01" };
+        var actor5 = new Actor { Name = "Tim Robbins", BirthDate = "1958-10-16" };
+        var actor6 = new Actor { Name = "Leonardo DiCaprio", BirthDate = "1974-11-11" };
+        var actor7 = new Actor { Name = "Joseph Gordon-Levitt", BirthDate = "1981-02-17" };
+        var actor8 = new Actor { Name = "Peter Stormare", BirthDate = "1953-08-27" };
         var actor9 = new Actor { Name = "Chuck Norris", BirthDate = "1940-03-10" };
         var actor10 = new Actor { Name = "Jennifer Aniston", BirthDate = "1969-02-11" };
         var actor11 = new Actor { Name = "Chevy Chase", BirthDate = "1943-10-08" };
-        var actor12 = new Actor { Name = "Jennifer Connely", BirthDate = "1970-12-12" };
-        var actor13 = new Actor { Name = "Kiera Knightley", BirthDate = "1985-03-26" };
+        var actor12 = new Actor { Name = "Jennifer Connelly", BirthDate = "1970-12-12" };
+        var actor13 = new Actor { Name = "Keira Knightley", BirthDate = "1985-03-26" };
         var actor14 = new Actor { Name = "Stellan Skarsgård", BirthDate = "1951-06-13" };
 
-
-
-        context.Actors.AddRange(actor1, actor2, actor3,actor4, actor5, actor6, actor7,
+        context.Actors.AddRange(actor1, actor2, actor3, actor4, actor5, actor6, actor7,
             actor8, actor9, actor10, actor11, actor12, actor13, actor14);
         
 
-        // 3. Skapa filmerna och bygg relationerna direkt i objekten
+        // 4. SKAPA FILMERNA OCH BYGG RELATIONERNA DIREKT
         var movies = new List<Movie>
         {
             new Movie
             {
                 Title = "The Matrix",
                 Year = 1999,
-                Genre = "Sci-Fi",
+                Genre = "Action / Sci-Fi",
                 Duration = 136,
-                // Lägg till 1:1 relationen direkt
                 Details = new MovieDetails
                 {
                     Synopsis = "A computer hacker learns from mysterious rebels about the true nature of his reality.",
                     Language = "English",
                     Budget = 63000000
                 },
-                // Lägg till 1:N relationen (Reviews)
                 Reviews = new List<Review>
                 {
                     new Review { Comment = "An absolute masterpiece of modern cinema!", Rating = 5, Reviewer = "Peter" },
                     new Review { Comment = "Great special effects for its time.", Rating = 4, Reviewer = "Anna" }
                 },
-                // Lägg till N:M relationen (Kopplingstabellen MovieActors)
                 MovieActors = new List<MovieActor>
                 {
                     new MovieActor { Actor = actor1 },
@@ -86,7 +72,7 @@ public static class DbSeeder
             {
                 Title = "Alien",
                 Year = 1979,
-                Genre = "Horror",
+                Genre = "Horror / Sci-Fi",
                 Duration = 117,
                 Details = new MovieDetails
                 {
@@ -153,7 +139,7 @@ public static class DbSeeder
             {
                 Title = "John Wick",
                 Year = 2014,
-                Genre = "Action",
+                Genre = "Action / Thriller",
                 Duration = 101,
                 Details = new MovieDetails
                 {
@@ -167,14 +153,83 @@ public static class DbSeeder
                 },
                 MovieActors = new List<MovieActor>
                 {
-                    // Här återanvänder vi Keanu Reeves (actor1) från förra exemplet!
                     new MovieActor { Actor = actor1 } 
                 }
+            },
+            // NY FILMER FÖR ATT REVOLVERA DE SKÅDESPELARE DU SKAPADE:
+            new Movie
+            {
+                Title = "National Lampoon's Christmas Vacation",
+                Year = 1989,
+                Genre = "Komedi",
+                Duration = 97,
+                Details = new MovieDetails
+                {
+                    Synopsis = "The Griswold family's plans for a big family Christmas predictably turn into a big disaster.",
+                    Language = "English",
+                    Budget = 25000000
+                },
+                MovieActors = new List<MovieActor> { new MovieActor { Actor = actor11 } }
+            },
+            new Movie
+            {
+                Title = "Good Will Hunting",
+                Year = 1997,
+                Genre = "Drama",
+                Duration = 126,
+                Details = new MovieDetails
+                {
+                    Synopsis = "Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life.",
+                    Language = "English",
+                    Budget = 10000000
+                },
+                MovieActors = new List<MovieActor> { new MovieActor { Actor = actor14 } } // Stellan Skarsgård
+            },
+            new Movie
+            {
+                Title = "A Beautiful Mind",
+                Year = 2001,
+                Genre = "Drama / Biografi",
+                Duration = 135,
+                Details = new MovieDetails
+                {
+                    Synopsis = "After John Nash, a brilliant but asocial mathematician, accepts secret work in cryptography, his life takes a turn for the nightmarish.",
+                    Language = "English",
+                    Budget = 58000000
+                },
+                MovieActors = new List<MovieActor> { new MovieActor { Actor = actor12 } } // Jennifer Connelly
+            },
+            new Movie
+            {
+                Title = "Pirates of the Caribbean: The Curse of the Black Pearl",
+                Year = 2003,
+                Genre = "Action / Äventyr",
+                Duration = 143,
+                Details = new MovieDetails
+                {
+                    Synopsis = "Blacksmith Will Turner teams up with eccentric pirate 'Captain' Jack Sparrow to save his love from Jack's former mutinous cohorts.",
+                    Language = "English",
+                    Budget = 140000000
+                },
+                MovieActors = new List<MovieActor> { new MovieActor { Actor = actor13 } } // Keira Knightley
+            },
+            new Movie
+            {
+                Title = "Fargo",
+                Year = 1996,
+                Genre = "Thriller / Crime / Komedi",
+                Duration = 98,
+                Details = new MovieDetails
+                {
+                    Synopsis = "Jerry Lundegaard's inept crime falls apart due to his and his henchmen's bungling and the persistent police work of pregnant Marge Gunderson.",
+                    Language = "English",
+                    Budget = 7000000
+                },
+                MovieActors = new List<MovieActor> { new MovieActor { Actor = actor8 } } // Peter Stormare
             }
-
         };
 
-        // 4. Spara allt i databasen
+        // 5. SPARA ALLT I DATABASEN VIA EN ENDA TRANSAKTION
         context.Movies.AddRange(movies);
         context.SaveChanges();
     }
